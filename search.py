e61451741e92670b1b37ca44181affdbb14ff30d
Brittany Martin <brittany.jill.martin@gmail.com>
2017-12-30 17:54:02 -0500

    Implemented the depth-first search (DFS) algorithm in the depthFirstSearch function in search.py

--- search.py
+++ search.py
@@ -13,6 +13,7 @@ by Pacman agents (in searchAgents.py).
 
 import util
 
+
 class SearchProblem:
   """
   This class outlines the structure of a search problem, but doesn't implement
@@ -20,13 +21,15 @@ class SearchProblem:
   
   You do not need to change anything in this class, ever.
   """
-  
+
+
   def getStartState(self):
      """
      Returns the start state for the search problem 
      """
      util.raiseNotDefined()
-    
+
+
   def isGoalState(self, state):
      """
        state: Search state
@@ -34,6 +37,7 @@ class SearchProblem:
      Returns True if and only if the state is a valid goal state
      """
      util.raiseNotDefined()
+
 
   def getSuccessors(self, state):
      """
@@ -46,6 +50,7 @@ class SearchProblem:
      cost of expanding to that successor
      """
      util.raiseNotDefined()
+
 
   def getCostOfActions(self, actions):
      """
@@ -67,24 +72,48 @@ def tinyMazeSearch(problem):
   w = Directions.WEST
   return  [s,s,w,s,w,w,s,w]
 
+
 def depthFirstSearch(problem):
-  """
-  Search the deepest nodes in the search tree first
-  [2nd Edition: p 75, 3rd Edition: p 87]
-  
-  Your search algorithm needs to return a list of actions that reaches
-  the goal.  Make sure to implement a graph search algorithm 
-  [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
-  
-  To get started, you might want to try some of these simple commands to
-  understand the search problem that is being passed in:
-  
-  print "Start:", problem.getStartState()
-  print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-  print "Start's successors:", problem.getSuccessors(problem.getStartState())
-  """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    """
+    Search the deepest nodes in the search tree first
+    [2nd Edition: p 75, 3rd Edition: p 87]
+    Your search algorithm needs to return a list of actions that reaches
+    the goal.  Make sure to implement a graph search algorithm
+    [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    from util import Stack
+
+    root = problem.getStartState()
+
+    # store the frontier on a Stack object
+    frontier = Stack()
+    # add the root node into the Stack
+    frontier.push((root, []))
+    # set is an unordered collection data type that is iterable, mutable, and has no duplicate elements
+    explored = set()
+
+    while not frontier.isEmpty():
+        # add note and its accompanying path to the frontier
+        node, path = frontier.pop()
+
+        # return the path if the goal state is reached
+        if problem.isGoalState(node):
+            return path
+
+        # if the node is not explored add to the explored path
+        if node not in explored:
+            explored.add(node)
+            for state, action, cost in problem.getSuccessors(node):
+                if state not in explored:
+                    frontier.push((state, path + [action]))
+
+    return []
+
 
 def breadthFirstSearch(problem):
   """
@@ -93,11 +122,13 @@ def breadthFirstSearch(problem):
   """
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
-      
+
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
+
 
 def nullHeuristic(state, problem=None):
   """
@@ -105,6 +136,7 @@ def nullHeuristic(state, problem=None):
   goal in the provided SearchProblem.  This heuristic is trivial.
   """
   return 0
+
 
 def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
